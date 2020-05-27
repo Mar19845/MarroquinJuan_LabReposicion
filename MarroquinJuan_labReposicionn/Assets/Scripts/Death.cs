6fb255044796d1f09836cfd7521217a550b9509e
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-26 20:16:42 -0600

    ya termindao

---
+++ MarroquinJuan_labReposicionn/Assets/Scripts/Death.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    public GameObject Canbas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    
+    public void GameOver()
+    {
+        Canbas.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ MarroquinJuan_labReposicionn/Assets/Scripts/FlyBirdFly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyBirdFly : MonoBehaviour
+{
+    public Death manager;
+    public float velocity;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+    
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        manager.GameOver();
+    }
+}
